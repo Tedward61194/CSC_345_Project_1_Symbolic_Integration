7385a8aaff094928d832ced03d222bba656d70e5
Segal <Ted.Segal@corebts.com>
2016-10-24 16:47:39 -0400

    Constructors complete

--- CSC_345_Project_1.lisp
+++ CSC_345_Project_1.lisp
@@ -71,12 +71,12 @@
 
 (defun negative-p (F)
   "t if F is negative"
-  (cond ((and (number-p F) (< F 0)) t)  ;;if a number and is less than 0
-	((number-p F) nil)              ;; clear all other predicate tags
+  (cond ((and (number-p F) (< F 0)) t)                            ;; if input is a number less then 0, t
+	((number-p F) nil)                                        ;; not if anything else
 	((variable-p F) nil)
 	((difference-p F) nil)
-	((and (equal (negative-operator F) negative-symbol)
-	      (not (equal (negative-operand F) negative-symbol)))
+	((and (equal (negative-operator F) negative-symbol)       ;; if the operator is the negative-symbol
+	      (not (equal (negative-operand F) negative-symbol))) ;; but not nested-negative
 	 t)))
 
 (defun nested-negative-p (F)
@@ -85,21 +85,104 @@
 				     (cond ((endp F) (= (length L) 1))
 				     ((equal (first F) negative-symbol) (nested-negative-p-helper (rest F) L))    ;; if First is '-' ...
 				     (t (nested-negative-p-helper (rest F) (cons (first F) L))))))                ;; return t and continue to cdr
-    ;; clear all other p-tags
     (cond ((number-p F) nil)
 	((variable-p F) nil)
 	((negative-p F) nil)
 	((difference-p F) nil)
 	((not (listp F)) nil)
-	(t (nested-negative-p-helper F '()))))
-  )
+	(t (nested-negative-p-helper F '())))))
+
+(defun sum-p (F)
+  "t if F is a sum func"
+  (cond ((number-p F) nil)
+	((variable-p F) nil)
+	((and (equal (sum-operator F) sum-symbol) ;; if the operator is the sum symbol
+	      (sum-operand-1 F)                   ;; the first operand will be sum-operand-1
+	      (sum-operand-2 F))                  ;; the second operand will be sum-operand-2
+	 t)))
+
+(defun difference-p (F)
+  "t if difference func"
+  (cond ((number-p F) nil)    ;; logic similar to sum-p
+	((variable-p F) nil)
+	((and (equal (difference-operator F) difference-symbol)
+	     (not (equal (difference-operand-1 F) difference-symbol))
+	     (difference-operand-2 F))
+	 t)))
+
+(defun power-p (F)
+  "t if expt funct"
+  (cond ((number-p F) nil)
+	((variable-p F) nil
+	 ((and (equal (power-operator F) power-symbol)) ;; if operator is the power-symbol
+	  (variable-p (poweroperand-1 F))               ;; the next 2 args are operands
+	  (number-p (power-operand-2 F)))
+	 t)))
+
 ;;--------------------------------------------
 
 ;; CONSTRUCTORS
-(defun make-variable (V) V)
+(defun make-variable (V)
+  "Trivial but included for completeness of the abstraction"
+  V)
 
 (defun make-sum (F G)
   (cond ((eq 0 F) G)                         ; 0+G = G
-	((eq 0G) F)                          ; F+0 = F
-	((and(numberp F)(numberp G))(+ F G)) ; if they are numbers, add them
-	( t (list sum-symbol FG))))
+	((eq 0 G) F)                          ; F+0 = F
+	((and(numberp F) (numberp G)) (+ F G)) ; if they are numbers, add them
+	( t (list sum-symbol F G))))
+
+(defun make-negative (F)
+  "Constructs negatative of arg"
+  (labels ((make-negative-helper (F)
+				 (cond ((number-p F) (* -1 F))
+				       ((negative-p F) (negative-operand F))
+				       (t (list negative-symbol F)))))
+    (cond ((nested-negative-p F) (make-negative-helper (make-negative-simplified F)))
+	  (t (make-negative-helper)))))
+
+(defun make-negative-simplified (F)
+  "simplify negative"
+  (labels ((make-negative-simplified-helper (F)
+					    (cond ((equal (mod (length F) 2) 0) (list negative-symbol (first (last F))))
+						  (t (first (last F))))))
+    (cond ((nested-negative-p F) (make-negative-simplified-helper F))
+	  ((negative-p F) F))))
+
+(defun make-difference (F G)
+  "Constructs difference of F and G"
+  (cond ((equal F 0) G)   ;; if F or G is 0, then the difference is the other
+	((equal G 0) F)
+	((equal F G) 0)   ;; X - X = 0
+	((and (number-p F) (number-p G)) (- F G))
+	((equal F (make-negative G)) (make-sum F (make-negative G))) ;; X - (-X) = X + (-(-X))
+	(t (list difference-symbol F G))))
+
+(defun make-product (F G)
+  "Constructs product of F and G"
+  (cond ((or (equal F 0)      ;; X * 0 = 0
+	     (equal G 0))
+	 0)
+	((equal F 1) G)  ;; X * 1 = X
+	((equal G 1) F)
+	((equal F -1) (make-negative G))  ;; X * -1 = -X
+	((equal G -1) (make-negative F))
+	((and (negative-p F) (negative-p G)) (make-product (make-negative F) (make-negative G))) ;; -1 * -1 = 1
+	((and (number-p F) (number-p G)) (* F G)) ;; not a special case
+	(t (list product-symbol F G))))
+
+(defun make-quotient (F G)
+  "Constructs quotient of F and G"
+  (cond ((equal F 0) 0)   ;; 0 / X = 0
+	((equal G 0) nil) ;; X / 0 = Undefined
+	((equal F G) 1)   ;; X / X = 1
+	((and (number-p F) (number-p G)) (/ F G))  ;; not a special case
+	(t (list quotient-symbol F G))))
+
+(defun make-log (V)
+  "Constructs log of V"
+  (cond ((variable-p V) (list log-symbol V))))
+
+(defun make-pwer (V N)
+  "Constructs V to the Nth power"
+  (cond ((and (variable-p V) (number-p N)) (list power-symbol V N))))
